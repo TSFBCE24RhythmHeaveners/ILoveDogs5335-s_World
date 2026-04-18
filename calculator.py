d632e3b4dfcb9abe406ac86e94e7013a02cc341b
MTPASLE763 On Github <zhygersamueloliquiano@gmail.com>
2026-04-18 16:49:09 +0400

    I made a simple Python calculator.

---
+++ calculator.py
@@ -0,0 +1,15 @@
+#Start code here
+number_1 = float(input("Enter the first number."))
+number_2 = float(input("Enter the second number."))
+add = number_1 + number_2
+subtract = number_1 - number_2
+multiply = number_1 * number_2
+divide = number_1 / number_2
+power = number_1 ** number_2
+root = number_2 ** (1 / number_1)
+print("Here!", number_1, "plus", number_2, "equals", add)
+print("Here!", number_1, "minus", number_2, "equals", subtract)
+print("Here!", number_1, "times", number_2, "equals", multiply)
+print("Here!", number_1, "divided by", number_2, "equals", divide)
+print("Here!", number_1, "to the", number_2, "equals", power)
+print("Here!", number_1, "'s root of", number_2, "equals", root)
