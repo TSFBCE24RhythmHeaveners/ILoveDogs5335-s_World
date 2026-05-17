3b382c7980672f49286bb151cbd06bdf1470838e
MTPASLE763 On Github <zhygersamueloliquiano@gmail.com>
2026-05-17 19:17:43 +0400

    Rewrite the calculator JScript partially.

--- javascripts/calculatorjs.js
+++ javascripts/calculatorjs.js
@@ -3,4 +3,4 @@ var number_1, number_2;
 
 number_1 = Number(window.prompt('Enter the first number.'));
 number_2 = Number(window.prompt('Enter the second number.'));
-window.alert(String(String('The addition is ' + String(String(number_1 + number_2) + '. ')) + String(String('The subtraction is ' + String(String(number_1 - number_2) + '. ')) + String('The multiplication is ' + String(String(number_1 * number_2) + '. ')))) + String(String('The division is ' + String(String(number_1 / number_2) + '. ')) + String(String('The exponentation is ' + String(String(Math.pow(number_1, number_2)) + '. ')) + String('The rooting is ' + String(String(Math.pow(number_1, 1 / number_2)) + '.')))));
+window.alert(['Addition result.' + String(String(number_1 + number_2) + '. '),'Subtraction result: ' + String(String(number_1 - number_2) + '. '),'Multiplication result: ' + String(String(number_1 * number_2) + '. '),'Division result: ' + String(String(number_1 / number_2) + '. '),'Exponentation result: ' + String(String(Math.pow(number_1, number_2)) + '. '),'Root result: ' + String(String(Math.pow(number_1, 1 / number_2)) + '.')].join(''));
